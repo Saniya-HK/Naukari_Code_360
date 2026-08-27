a5b27b55260d11a1910e6ab7d88354e2f5fef7f1
SANIYA H K <hksaniya717@gmail.com>
2026-08-27 18:44:00 +0530

    Dutch National Flag Algo
    Implement Dutch National Flag algorithm to sort an array of 0s, 1s, and 2s.

---
+++ Array/sort012.java
@@ -0,0 +1,32 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution 
+{
+    public static void swap(int[] arr,int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public static void sort012(int []arr)
+    {
+        //Write your code here
+        int low=0;
+        int mid=0;
+        int high =arr.length-1;
+        for (int i=0; i<arr.length;i++){
+            if (arr[mid]==0){
+                swap(arr,low,mid);
+                mid++;
+                low++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else {
+                swap(arr,mid,high);
+                high--;
+            }
+        }
+    }
+}
