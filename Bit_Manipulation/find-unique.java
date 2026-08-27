9c67b896f4b436c72eb6e133ba3805cdde287b16
SANIYA H K <hksaniya717@gmail.com>
2026-08-27 18:41:05 +0530

    Add findUnique method for unique number detection
    Implement findUnique method to find the unique number in an array using XOR.

---
+++ Bit_Manipulation/find-unique.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution{  
+
+    public static int findUnique(int[] arr){
+		//Your code goes here
+    int xor=0;
+    for (int i=0; i<arr.length; i++){
+      xor= xor^arr[i];
+    }
+    return xor;
+    }
+}
